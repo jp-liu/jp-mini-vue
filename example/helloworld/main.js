f3c35288c9adad54996a9f89ed5b00d5a325595d
jpliu6 <jpliu6@iflytek.com>
2021-12-15 00:00:53 +0800

    ✨特性(runtime-core): ✨特性(runtime-core): render dom to browser
    实现最简单版本,渲染dom到浏览器上

--- example/helloworld/main.js
+++ example/helloworld/main.js
@@ -1,4 +1,5 @@
-import App from './app.js'
+import { App } from './app.js'
 import { createApp } from '../../lib/jp-liu-mini-vue.esm.js'
 
-const app = createApp(App).mount('#app')
+const rootContainer = document.getElementById('app')
+const app = createApp(App).mount(rootContainer)
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -5,11 +5,15 @@ export function render(vnode, rootContainer: HTMLElement | string) {
 }
 
 function patch(vnode, container) {
+  debugger
   // 1.判断类型,进行不同操作
-  // 1.1 组件
-  processComponent(vnode, container)
-
-  // 1.2 TODO 元素
+  if (typeof vnode.type === 'string') {
+    // 1.1 TODO 元素
+    processElement(vnode, container)
+  } else {
+    // 1.2 组件
+    processComponent(vnode, container)
+  }
 }
 
 function processComponent(vnode: any, container: any) {
@@ -34,4 +38,22 @@ function setupRenderEffect(instance, container) {
 
   // 2.继续`patch`,递归挂载组件
   patch(subTree, container)
+}
+
+function processElement(vnode: any, container: any) {
+  // 1.挂载元素
+  mountElement(vnode, container)
+}
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+
+  const { children } = vnode
+  el.textContent = children
+
+  const { props } = vnode
+  for (const key in props) {
+    el.setAttribute(key, props[key])
+  }
+
+  container.append(el)
 }
