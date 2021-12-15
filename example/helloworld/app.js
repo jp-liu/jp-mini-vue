3b6fccb5170c0d176013011d49126c87a84fd41d
jpliu6 <jpliu6@iflytek.com>
2021-12-15 23:11:00 +0800

    ✨特性(runtime-core): support array vnode children render

--- example/helloworld/app.js
+++ example/helloworld/app.js
@@ -3,7 +3,11 @@ import { h } from '../../lib/jp-liu-mini-vue.esm.js'
 export const App = {
   // 暂时没有编译能力,使用手动的 `render` 函数
   render() {
-    return h('div', { id: 'wahaha', class: 'red' }, 'hello mini-vue')
+    // return h('div', { id: 'wahaha', class: 'red' }, 'hello mini-vue')
+    return h('div', { id: 'wahaha', class: 'red' }, [
+      h('span', { class: 'blue' }, 'hello'),
+      h('span', { class: 'red' }, 'mini-vue')
+    ])
   },
 
   setup() {
