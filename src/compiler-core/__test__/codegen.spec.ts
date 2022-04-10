efbf2b42ae2b63af3d2f7d9e2674522484a1df11
jpliu <fortune_coder@foxmail.com>
2022-04-10 23:56:20 +0800

    ✅测试(compiler-core): 编译元素类型
    添加一个元素类型测试用例,TDD开发

--- src/compiler-core/__test__/codegen.spec.ts
+++ src/compiler-core/__test__/codegen.spec.ts
@@ -1,7 +1,9 @@
 import { generate } from "../src/codegen";
 import { baseParse } from "../src/parse";
 import { transform } from "../src/transform";
+import { transformElement } from "../src/transforms/transformElement";
 import { transformExpress } from "../src/transforms/transformExpress";
+import { transformText } from "../src/transforms/transformText";
 
 describe('codegen', () => {
   it('should gen for string', () => {
@@ -23,6 +25,17 @@ describe('codegen', () => {
     // 快照测试,
     // 1.有意更新
     // 2.错误对比
+    expect(code).toMatchSnapshot()
+  });
+
+  it('should gen for element', () => {
+    const ast = baseParse('<div>hi,{{message}}</div>')
+    transform(ast, {
+      nodeTransforms: [transformExpress, transformElement, transformText]
+    })
+    console.log('AAAA',(ast as any).codegenNode.children);
+    
+    const { code } = generate(ast)
     expect(code).toMatchSnapshot()
   });
 });
